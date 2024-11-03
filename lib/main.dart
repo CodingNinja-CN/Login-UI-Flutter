7cd5d1148d658266bba903cb64cd34389ded8a78
Coding Ninja <cn.codingninja@gmail.com>
2024-11-03 20:11:27 +0500

    Initial
    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+
+              //Logo
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Image(image: AssetImage("assets/logo.png")),
+                    width: 70,
+                    height: 70,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Maintainance",
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Box",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffda6220)),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 60,
+              ),
+
+              //Login title
+              Text(
+                "Log in",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+
+              //Login subheading
+              Text(
+                "Log in to your personalized Maintainace\n Box and maintain everything easily.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+
+              //Input fields
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal:20.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,prefixIcon: Icon(Icons.email),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:20.0,right: 20,left: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.visibility_off_outlined),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 200,
+              ),
+
+              //Login Button
+              Container(
+                width: 300,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xffda6220),
+                ),
+                child: Center(
+                  child: Text(
+                    "LOG IN",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+
+              //Don't have an account? Sign Up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account? ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    "Sign Up",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xffda6220),
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
